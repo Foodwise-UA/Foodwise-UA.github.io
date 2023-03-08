a6dfd203500cd18a309a2da5f5f1f6159f7167d4
Sanya210904 <112613357+Sanya210904@users.noreply.github.com>
2023-03-08 09:44:58 +0200

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -5,7 +5,7 @@ document.querySelectorAll('a.yakor').forEach((link) => {
     const href = this.getAttribute('href').substring(1);
     const scrollTarget = document.getElementById(href);
 
-    const offsetFromTop = 130;
+    const offsetFromTop = 40;
 
     const blockTopPosition = scrollTarget.getBoundingClientRect().top;
     let offset = blockTopPosition - offsetFromTop;
@@ -18,21 +18,39 @@ document.querySelectorAll('a.yakor').forEach((link) => {
 });
 
 let topPosition = document.scrollY;
-let navBar = document.querySelector('nav.header__nav')
-console.log(navBar);
+let navBar = document.querySelector('nav.header__nav');
 
-window.addEventListener('scroll', function(event) {
-    event.preventDefault();
+window.addEventListener('scroll', function (event) {
+  event.preventDefault();
 
-    let currentPosition = this.scrollY;
+  let currentPosition = this.scrollY;
 
-    if (currentPosition > topPosition) {
-        navBar.classList.remove('active');
-        console.log('to down')
-    } else {
-        navBar.classList.add('active');
-        console.log('to top');
-    }
+  if (currentPosition > topPosition) {
+    navBar.classList.remove('active');
+  } else if (currentPosition < topPosition) {
+    navBar.classList.add('active');
+  }
 
-    topPosition = currentPosition;
-})
+  topPosition = currentPosition;
+});
+
+// const form = document.querySelector('.contact__form');
+// let emailInput = document.querySelector('.email-input');
+// console.log(emailInput);
+
+// form.addEventListener('onsubmit', function (event) {
+//   event.preventDefault();
+
+  
+//   fetch("https://api.apispreadsheets.com/data/Q1rx25WRYI3qcd4J/", {
+// 	method: "POST",
+// 	body: JSON.stringify({"data": {"Email":"value"}}),
+// }).then(res =>{
+// 	if (res.status === 201){
+// 		alert('OK')
+// 	}
+// 	else{
+// 		alert('Fail')
+// 	}
+// })
+// });
