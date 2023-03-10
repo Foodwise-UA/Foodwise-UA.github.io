47a16e6995d776cb6425d93ee42c18b926606358
Sanya210904 <112613357+Sanya210904@users.noreply.github.com>
2023-03-10 10:58:52 +0200

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -1,64 +1,76 @@
-let navBar = document.querySelector('nav.header__nav');
+const API_URL = 'https://api.apispreadsheets.com/data/Q1rx25WRYI3qcd4J/';
 
-document.querySelectorAll('a.yakor').forEach((link) => {
-  link.addEventListener('click', function (event) {
-    event.preventDefault();
 
-    const href = this.getAttribute('href').substring(1);
-    const scrollTarget = document.getElementById(href);
 
-    const offsetFromTop = 40;
+function smoothScroll(offsetFromTop = 40) {
+  document.querySelectorAll('a.yakor').forEach((link) => {
+    link.addEventListener('click', function (event) {
+      event.preventDefault();
 
-    const blockTopPosition = scrollTarget.getBoundingClientRect().top;
-    let offset = blockTopPosition - offsetFromTop;
+      const href = this.getAttribute('href').substring(1);
+      const scrollTarget = document.getElementById(href);
 
-    window.scrollBy({
-      top: offset,
-      behavior: 'smooth',
+      const blockTopPosition = scrollTarget.getBoundingClientRect().top;
+      let offset = blockTopPosition - offsetFromTop;
+
+      window.scrollBy({
+        top: offset,
+        behavior: 'smooth',
+      });
     });
   });
-});
+}
 
+function changeNavBar() {
+  let navBar = document.querySelector('nav.header__nav');
+  let topPosition = document.scrollY;
 
-let topPosition = document.scrollY;
+  window.addEventListener('scroll', function (event) {
+    event.preventDefault();
 
-window.addEventListener('scroll', function (event) {
-  event.preventDefault();
+    let currentPosition = this.scrollY;
 
-  let currentPosition = this.scrollY;
+    if (currentPosition > topPosition && currentPosition > 90) {
+      navBar.classList.remove('active');
+    } else if (currentPosition < topPosition) {
+      navBar.classList.add('active');
+    }
 
-  if (currentPosition > topPosition && currentPosition > 90) {
-    navBar.classList.remove('active');
-  } else if (currentPosition < topPosition) {
-    navBar.classList.add('active');
-  }
+    if (currentPosition > 50) {
+      navBar.classList.add('shadow');
+    } else {
+      navBar.classList.remove('shadow');
+    }
 
-  if (currentPosition > 50) {
-    navBar.classList.add('shadow');
-  } else {
-    navBar.classList.remove('shadow');
-  }
+    topPosition = currentPosition;
+  });
+}
 
-  topPosition = currentPosition;
-});
+function getUserEmail() {
+  const emailInput = document.querySelector('.email-input');
+  const formButton = document.querySelector('.form-button');
 
-// const form = document.querySelector('.contact__form');
-// let emailInput = document.querySelector('.email-input');
-// console.log(emailInput);
+  formButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    let emailInputValue = emailInput.value;
 
-// form.addEventListener('onsubmit', function (event) {
-//   event.preventDefault();
+    fetch(API_URL, {
+      method: 'POST',
+      body: JSON.stringify({ data: { Email: emailInputValue } }),
+    }).then((res) => {
+      if (res.status === 201) {
+        formButton.classList.add('success');
+        formButton.innerHTML = 'Success';
+      } else {
+        formButton.classList.add('error');
+        formButton.innerHTML = 'Error. Please, refresh page';
+      }
+    });
+  });
+}
 
-  
-//   fetch("https://api.apispreadsheets.com/data/Q1rx25WRYI3qcd4J/", {
-// 	method: "POST",
-// 	body: JSON.stringify({"data": {"Email":"value"}}),
-// }).then(res =>{
-// 	if (res.status === 201){
-// 		alert('OK')
-// 	}
-// 	else{
-// 		alert('Fail')
-// 	}
-// })
-// });
+window.onload = function () {
+  smoothScroll();
+  changeNavBar();
+  getUserEmail();
+};
