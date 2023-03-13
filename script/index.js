fa05a1ef196c76d4542aebc15c95b54cc4b3976b
Sanya210904 <112613357+Sanya210904@users.noreply.github.com>
2023-03-13 11:44:33 +0200

    Validating email, changed contact block

--- script/index.js
+++ script/index.js
@@ -1,7 +1,5 @@
 const API_URL = 'https://api.apispreadsheets.com/data/Q1rx25WRYI3qcd4J/';
 
-
-
 function smoothScroll(offsetFromTop = 40) {
   document.querySelectorAll('a.yakor').forEach((link) => {
     link.addEventListener('click', function (event) {
@@ -46,26 +44,41 @@ function changeNavBar() {
   });
 }
 
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
 function getUserEmail() {
   const emailInput = document.querySelector('.email-input');
   const formButton = document.querySelector('.form-button');
+  const emailValidatorText = document.querySelector('.email-validator-text')
 
   formButton.addEventListener('click', function (event) {
     event.preventDefault();
     let emailInputValue = emailInput.value;
+    const isValidEmail = validateEmail(emailInputValue);
 
-    fetch(API_URL, {
-      method: 'POST',
-      body: JSON.stringify({ data: { Email: emailInputValue } }),
-    }).then((res) => {
-      if (res.status === 201) {
-        formButton.classList.add('success');
-        formButton.innerHTML = 'Success';
-      } else {
-        formButton.classList.add('error');
-        formButton.innerHTML = 'Error. Please, refresh page';
-      }
-    });
+    if (isValidEmail) {
+      emailValidatorText.classList.remove('active');
+      fetch(API_URL, {
+        method: 'POST',
+        body: JSON.stringify({ data: { Email: emailInputValue } }),
+      }).then((res) => {
+        if (res.status === 201) {
+          formButton.classList.add('success');
+          formButton.innerHTML = 'Success';
+        } else {
+          formButton.classList.add('error');
+          formButton.innerHTML = 'Error. Please, refresh page';
+        }
+      });
+    } else {
+      emailValidatorText.classList.add('active');
+    }
   });
 }
 
